2ba9b314a329af266e2bfa32ec7ec28a9cc7d9a2
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-02-23 22:01:01 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,42 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Home({ data: {slip} }) {
+  const { id, advice } = slip;
+  const router = useRouter()
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-[#323a49] h-screen flex font-body items-center justify-center">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className='bg-[#4e5d73] w-[90%] md:w-1/2 p-9 shadow-xl rounded-xl text-center'>
+        <div>
+          <h5 className='mb-3 text-[#52ffa8] uppercase font-semibold'>Advice #{id}</h5>
+          <p className='text-white text-[22px] font-semibold p-3'>
+            "
+            {advice}
+            "
+          </p>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+          <div className='border border-gray-300 mb-5' />
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className='hover:bg-gray-200/40 p-3 w-12 mx-auto rounded-full'>
+            <img src="/images/icon-dice.svg" alt="icon-dice" className='mx-auto cursor-pointer' onClick={() => router.push('/')} />
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
+};
+
+export async function getServerSideProps() {
+  const data = await fetch('https://api.adviceslip.com/advice').then(res => res.json())
+
+  return {
+    props: {
+      data,
+    }
+  }
 }
